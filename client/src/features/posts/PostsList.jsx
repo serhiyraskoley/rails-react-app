cc5ffb21606bb0a81fdaf00903b8e4bb6882bdb4
serhiyraskoley <r.serogka@gmail.com>
2023-09-10 19:14:21 +0300

    Implemented POST page

--- client/src/features/posts/PostsList.jsx
+++ client/src/features/posts/PostsList.jsx
@@ -31,9 +31,8 @@ function PostsList() {
     <div>
       {posts.map((post) => (
         <div key={post.id} className="post-container">
-          <h2 class="post-title">{post.title}</h2>
-          <p class="post-body">{post.body}</p>
-          {/* <p className="post-date">{Date(post.created_at).split("GMT+0300 (Eastern European Summer Time)")}</p> */}
+          <h2 className="post-title">{post.title}</h2>
+          <p className="post-body">{post.body}</p>
           <p className="post-date">{Date(post.created_at).split(/[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\s.+/)}</p>
         </div>
       ))}
